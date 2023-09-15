414781c6cf1c8f752809e234aaf8ffcde7e216ae
Raphael Tholl <raphael.tholl@ibm.com>
2023-09-15 10:26:16 +0200

    added saving animation

--- src/components/CoffeeAdd/CoffeeAdd.js
+++ src/components/CoffeeAdd/CoffeeAdd.js
@@ -10,43 +10,82 @@ import {
   RadioButton,
   Button,
   Heading,
+  InlineLoading,
 } from "@carbon/react";
-
-const handleSubmit = async (event) => {
-  event.preventDefault();
-
-  console.log(event);
-  const data = {
-    roaster: event.target[1].value,
-    bean: event.target[2].value,
-    input: event.target[3].value,
-    output: event.target[6].value,
-    mill: event.target[9].value,
-    time: event.target[12].value,
-    temperature: event.target[15].value,
-    "18g": event.target[19].checked,
-    "12g": event.target[20].checked,
-  };
-
-  const JSONdata = JSON.stringify(data);
-
-  const endpoint = "/api/v1/add";
-
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSONdata,
-  };
-
-  const response = await fetch(endpoint, options);
-
-  const result = await response.json();
-  console.log(result);
-};
+import { useState } from "react";
 
 export default function CoffeeAdd() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [savingStatus, setSavingStatus] = useState("finished");
+  const [savingMessage, setSavingMessage] = useState("Saving recipe...");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setSavingMessage("Saving recipe...");
+    setSavingStatus("active");
+    setIsLoading(true);
+
+    const data = {
+      roaster: event.target[1].value,
+      bean: event.target[2].value,
+      input: event.target[3].value,
+      output: event.target[6].value,
+      mill: event.target[9].value,
+      time: event.target[12].value,
+      temperature: event.target[15].value,
+      "18g": event.target[19].checked,
+      "12g": event.target[20].checked,
+    };
+
+    const JSONdata = JSON.stringify(data);
+
+    const endpoint = "/api/v1/add";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+
+    const result = await response.json();
+
+    if (response.ok) {
+      setSavingStatus("finished");
+      setSavingMessage("Successfully saved recipe!");
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    } else {
+      setSavingStatus("error");
+      setSavingMessage("Failed to save recipe!");
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    }
+  };
+
+  let savingAction = {};
+  if (!isLoading) {
+    savingAction = (
+      <Button type="submit" className="coffee-add-save">
+        Save
+      </Button>
+    );
+  } else {
+    savingAction = (
+      <InlineLoading
+        status={savingStatus}
+        iconDescription="saving"
+        description={savingMessage}
+      />
+    );
+  }
+
   return (
     <Grid>
       <Column lg={16} md={8} sm={4}>
@@ -166,9 +205,7 @@ export default function CoffeeAdd() {
               </Grid>
               <Grid>
                 <Column lg={16} md={8} sm={4}>
-                  <Button type="submit" className="coffee-add-save">
-                    Save
-                  </Button>
+                  {savingAction}
                 </Column>
               </Grid>
             </Stack>
